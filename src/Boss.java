07489317f62db0e5cb3306361b77ca49eda50cea
Saule0006 <mambetalievasaule40@gmail.com>
2025-03-03 20:00:16 +0600

    gg

--- src/Boss.java
+++ src/Boss.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
-
 public class Boss {
+
+    private int health;
+    private int damage;
+    private String dafance;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getDafance() {
+        return dafance;
+    }
+
+    public void setDafance(String dafance) {
+        this.dafance = dafance;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Boss boss = new Boss();
+        boss.setHealth(1000);
+        boss.setDamage(1000);
+        boss.setDafance("armor");
+
+        System.out.println("Boss health: " + boss.getHealth() + " damage: " + boss.getDamage() + " dafance: " + boss.getDafance());
+        Hero[] heroes = createHeroes();
+        for (Hero hero: heroes){
+            System.out.println("Hero health: " + hero.getHealth() + " damage: " + hero.getDamage() + " superAbility " + hero.getSuperAbility());
         }
+    }
+
+    public  static  Hero[] createHeroes(){
+        Hero hero1 = new Hero(100,12);
+        Hero hero2 = new Hero(123, 45,"magic");
+        Hero hero3 = new Hero(100,12);
+        Hero[]  heroes = {hero1,hero2,hero3};
+        return heroes;
     }
 }
